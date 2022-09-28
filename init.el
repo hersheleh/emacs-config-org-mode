2b73ca2bc5c8e191adecb819d01c22f27d1a940a
Grisha <hersheleh@gmail.com>
2022-09-27 17:17:02 -0700

    org-mode: customized. added src emacs-lisp shortcut
    eg. > C-c C-, el <RET>:
    
        \#+begin_src emacs-lisp
    
        \#+end_src

--- init.el
+++ init.el
@@ -7,7 +7,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (doom-modeline use-package))))
+ '(org-structure-template-alist
+   '(("a" . "export ascii")
+     ("c" . "center")
+     ("C" . "comment")
+     ("e" . "example")
+     ("E" . "export")
+     ("h" . "export html")
+     ("l" . "export latex")
+     ("q" . "quote")
+     ("s" . "src")
+     ("v" . "verse")
+     ("el" . "src emacs-lisp")))
+ '(package-selected-packages '(company company-mode doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
