7d144f9e0e3d55c33df389eef963a8d6e012b74a
Grisha <hersheleh@gmail.com>
2022-09-30 10:39:01 -0700

    configured python-lsp. installed vterm. customized hightlight:orange

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(company-minimum-prefix-length 2)
+ '(doom-molokai-brighter-comments t)
  '(org-structure-template-alist
    '(("a" . "export ascii")
      ("c" . "center")
@@ -22,7 +23,7 @@
      ("v" . "verse")
      ("el" . "src emacs-lisp")))
  '(package-selected-packages
-   '(dap-mode page-break-lines all-the-icons org-superstar company company-mode doom-modeline use-package)))
+   '(vterm dap-mode page-break-lines all-the-icons org-superstar company company-mode doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -30,7 +31,7 @@
  ;; If there is more than one, they won't work right.
  '(counsel--mark-ring-highlight ((t (:inherit highlight))))
  '(ivy-current-match ((t (:background "#fd971f" :foreground "black"))))
- '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
  '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
  '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
