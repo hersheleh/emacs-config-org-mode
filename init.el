f58a5a0996f39933d0be07cb7281bede4dba2213
Grisha <hersheleh@gmail.com>
2022-10-05 12:24:23 -0700

    installed; beacon. org-roam. helpful.

--- init.el
+++ init.el
@@ -23,11 +23,16 @@
      ("v" . "verse")
      ("el" . "src emacs-lisp")))
  '(package-selected-packages
-   '(ivy-rich rainbow-delimiters vterm dap-mode page-break-lines all-the-icons org-superstar company company-mode doom-modeline use-package)))
+   '(beacon baeacon org-roam helpful ivy-rich rainbow-delimiters vterm dap-mode page-break-lines all-the-icons org-superstar company company-mode doom-modeline use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(counsel--mark-ring-highlight ((t (:inherit highlight))))
- '(ivy-current-match ((t (:background "#fd971f" :foreground "black")))))
+ '(ivy-current-match ((t (:background "#fd971f" :foreground "black"))))
+ '(org-level-1 ((t (:inherit outline-1 :height 1.4))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
