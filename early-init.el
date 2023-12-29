0ae5d892e482a4d0d4baa10cb9932addf63cca70
Grisha <hersheleh@gmail.com>
2023-12-29 13:59:58 -0800

    remove init.el and early-init.el from git

--- early-init.el
+++
@@ -1,8 +0,0 @@
-;; Set Garbage collection threshold very high at startup
-(setq gc-cons-threshold (* 100 1000 1000))
-;; For lsp-mode:
-;;  Increase the amount of data which Emacs reads from the process
-(setq read-process-output-max (* 1024 1024))
-
-(defvar default-file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
